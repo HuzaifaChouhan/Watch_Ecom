b113015ddf44701bf09aea0d42b0971dcacb412e
HuzaifaChouhan <huzefachouhan07@gmail.com>
2025-04-10 17:56:16 +0530

    main

--- src/watch-website/watchwala.jsx
+++ src/watch-website/watchwala.jsx
@@ -3,12 +3,14 @@ import Header from './header/header';
 // import { BrowserRouter } from 'react-router-dom';
 // import { BrowserRouter ,Routes, Route, NavLink } from 'react-router-dom';
 import backgroundImage from '../image/background-image.png';
+import Main from './main/main';
 
 export default function Watchwala(){
     return<>
     <div className="main" style={{ backgroundImage: `url(${backgroundImage})` }}>
     <Header/>
     </div>
+    <Main/>
     {/* <BrowserRouter>
     <Routes>
         <Route path='/' element={<Home/>} />
